c6e77a071c8ef1805d53c14b340b715a1a489a31
asciidea <53179496+asciidea@users.noreply.github.com>
2022-08-19 09:25:27 -0300

    Adding Splah and Fade

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ function SetupGame()
 	//setip game object
 
 	Game.ESTATE_SPLASH = 0
+	Game.ESTATE_MENU = 1
 
 	Game.name = "Game Name"
 	Game.version = "0.0.1"
@@ -13,6 +14,13 @@ function SetupGame()
 
 	Game.estate = Game.ESTATE_SPLASH
 	Game.framesPerSecs = 128.0
+	Game.fadeScale = 0.0
+	Game.splashTime = 3
+
+	setTimeout(function(){
+		Game.fadeScale = 1.0
+		Game.estate = Game.ESTATE_MENU
+	}, 1000 * Game.splashTime)
 
 	frameRate(Game.framesPerSecs)
 }
@@ -35,12 +43,22 @@ function draw()
 {
 	//draw func
 
-	background(55)
+	if (Game.fadeScale > 0.0)
+	{
+		Game.fadeScale -= 0.05
+	}
+
+	background(0)
 
 	if (Game.estate === Game.ESTATE_SPLASH)
 	{
 		//On Estate Splash
 	}
+
+	push()
+	fill(255, 255, 255, 255 * Game.fadeScale)
+	rect(0, 0, width, height)
+	pop()
 
 	push()
 	fill(255)
